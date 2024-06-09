906228fecf6e7fd4d565725b66a53e381945f09d
cjmac2099 <cjmac2017@gmail.com>
2024-06-09 14:47:56 -0700

    images added

--- poker/src/app/page.js
+++ poker/src/app/page.js
@@ -1,8 +1,35 @@
+import Image from 'next/image';
 import React from 'react';
+import '@/assets/images/AceSpades.png';
 
 export default function Home() {
   return (
     <>
+    {/* Header */}
+    <header>
+      <div className="header-title">
+        <h1 className="text-2xl font-bold"> Late Night Poker </h1>
+      </div>
+    </header>
+
+    {/* Content / Logic */}
+    <main>
+      {/* CARDS */}
+      <div className="card-container">
+        <div className="card">
+          <div className="card-inner">  {/* INNER */}
+            <div className="card-front">  {/* FRONT */}
+              <Image src="" alt="" className="card-img" />
+            </div>
+
+            <div className="card-back">  {/* BACK */}
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+      
     </>
   );
 }
